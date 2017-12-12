a2f7553d741812888a1e5eada5715289624a94da
Technifutur <600@technifutur.be>
2017-12-12 12:37:37 +0100

    First commit

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : DualBehaviour
+{
+    #region Public Members
+
+    public int m_myFirstInt;//les variables public se mettent en haut
+                            //m_ pour atteindre la famille de cette classe
+    #endregion
+
+    #region Publuc void
+
+    #endregion
+
+    #region system
+
+    void Start () //pas de logique dans start et update
+    {
+		
+	}
+	
+	
+	void Update () 
+    {
+		
+	}
+
+    #endregion
+
+    #region Tools Debug and Utility
+
+    #endregion
+
+    #region Private and Protected Members
+
+    private float m_myFirstFloat;
+
+    #endregion
+}
